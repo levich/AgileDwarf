d3e8b6636d9c0a3823eae7df40fbba8e609cb842
Allen Wei <digruby@gmail.com>
2013-02-25 10:04:40 +0800

    fix text edit box

--- assets/javascripts/libs/jquery.jeditable.ptext.js
+++ assets/javascripts/libs/jquery.jeditable.ptext.js
@@ -1,13 +1,15 @@
-$.editable.addInputType('ptext', {
+(function($) {
+  $.editable.addInputType('ptext', {
     element : function(settings, original)
     {
-        var input = $('<input type="text"/>');
-        if (settings.width  != 'none') { input.attr('width', settings.width);  }
-        if (settings.height != 'none') { input.attr('height', settings.height); }
-        input.attr('autocomplete','off');
-        input.attr('placeholder', settings.placeholder);
-        $(this).append(input);
-        input.textPlaceholder();
-        return input;
+      var input = $('<input type="text"/>');
+      if (settings.width  != 'none') { input.attr('width', settings.width);  }
+      if (settings.height != 'none') { input.attr('height', settings.height); }
+      input.attr('autocomplete','off');
+      input.attr('placeholder', settings.placeholder);
+      $(this).append(input);
+      input.textPlaceholder();
+      return input;
     }
-});
+  });
+})(Query);
