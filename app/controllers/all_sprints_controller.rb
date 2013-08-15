60c5bcb344c0fa7b9de7fd6dbe62e1dbea14f8a3
ansonK <ansonkelly@gmail.com>
2013-08-15 16:48:36 +0100

    group all user stories for the same release together and not separate by project on all_sprints view

--- app/controllers/all_sprints_controller.rb
+++ app/controllers/all_sprints_controller.rb
@@ -3,11 +3,15 @@ class AllSprintsController < ApplicationController
 
   def index
     @backlog = SprintsTasks.get_backlog.group_by(&:project_id).each {|project_id, sprint_tasks| sprint_tasks.sort!{|a,b| a.parent_id || 0 <=> b.parent_id || 0 } }
-    @sprints = Sprints.all_sprints.group_by(&:project_id)
-    # @sprints.each{|s| s['tasks'] = SprintsTasks.get_tasks_by_sprint(@project, [s.id])}
-    # @assignables = {}
-    # @project.assignable_users.each{|u| @assignables[u.id] = u.firstname + ' ' + u.lastname}
-    # @project_id = @project.id
+    
+    @sprints = Sprints.all_sprints.select {|s| s.name.downcase.match(/release$/).nil? }
+    @sprints.each {|s| s.tasks = SprintsTasks.get_tasks_by_sprint(nil, s.id) }
+    @sprints = @sprints.group_by(&:project_id)
+
+    @releases = Sprints.all_sprints.select {|s| s.name.downcase.match(/release$/).present? }
+    @releases.each {|s| s.tasks = SprintsTasks.get_tasks_by_sprint(nil, s.id) }
+    puts @releases.inspect
+
     @plugin_path = File.join(Redmine::Utils.relative_url_root, 'plugin_assets', 'agile_dwarf')
     @closed_status = Setting.plugin_agile_dwarf["stclosed"].to_i
   end
